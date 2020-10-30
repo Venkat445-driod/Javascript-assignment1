f62f6497dbf51471eeed650e78866140061d534f
Venkat445-driod <71118228+Venkat445-driod@users.noreply.github.com>
2020-10-30 16:11:51 +0530

    Add files via upload

---
+++ problem2.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>
+                    Current Number
+                </title>
+    </head>
+    <body>
+        <script>
+          let n=promt("enter a number")
+          for(i=2;i<5;i++){
+          if (i=2){
+          console.log("even number")
+                }
+                if(i=3){
+                    console.log("odd number")
+                }
+                if (i=4){
+                    console.log("prime number")
+                }
+            }
+        </script>
+    </body>
+</html>
+
+
+
+
